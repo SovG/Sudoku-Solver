23d30224c3a518ecd185a20c5894c2d46d63a7db
SovG <nehal.ghuman@gmail.com>
2017-04-22 11:09:12 +0800

    Added memAlloc.c No major changes

---
+++ memAlloc.c
@@ -0,0 +1,3 @@
+/* File contains functions related to the creation, management, and deletion
+ * of Shared Memory areas as required by the Multi-Process part of the OS
+ * assignment                                                             */
