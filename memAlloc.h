cac69242c2e762fd6013a9d799313f8c8fc2bd3e
SovG <nehal.ghuman@gmail.com>
2017-04-22 20:50:25 +0800

    Finished for night No Major Changes

---
+++ memAlloc.h
@@ -0,0 +1,15 @@
+/* File contains function prototypes and data types for Buffer1 and Buffer2 */
+
+#IFNDEF MEMALLOC_H
+#DEF MEMALLOC_H
+typedef struct Buffer1
+{
+    int** sudokuSolution;
+} Buffer1;
+
+typedef struct Buffer2
+{
+    int* validated
+}
+
+#ENDIF
